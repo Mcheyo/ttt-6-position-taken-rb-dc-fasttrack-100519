05d3bcb14eae769dbf33817634c04350eb244a68
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-10-06 14:05:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+#return truoe or flase depending..
+if board[index] == " " || board index == "" || board[index ==nil]
+  return false
+else
+  return true
+end
+
+end
+game_board = [ "X", " ", " 0", " ", " ", " ", " ", " ", " ", ]
+
+position_taken?(game_board, 2)
